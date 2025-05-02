d9014ec79d680dc643ddd5ca03a5ce38befcbf15
1fHu <simonhu125@gmail.com>
2025-05-01 23:07:26 -0400

    Used correct FFT, this version for test frequency energy

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <Adafruit_CircuitPlayground.h>
-#include "Adafruit_ZeroFFT.h"
+#include <Arduino.h>
+#include <arduinoFFT.h>
+
 
 float X, Y, Z;
 
-//this must be a power of 2
-#define DATA_SIZE 256
+// not larger than 128
+#define SAMPLES 128
 
 #define SENSOR_RANGE 20
+#define SAMPLING_FREQ 100 // Hz, determined by last delay in loop 
 
-int16_t inputData[DATA_SIZE];
+// variables in FFT
+double real[SAMPLES], imag[SAMPLES];
+
+// FFT itself
+ArduinoFFT<double> FFT;
+
+
+// int16_t inputData[sample];
 int i = 0;
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial);  
   CircuitPlayground.begin();
 }
 
@@ -23,34 +34,48 @@ void loop() {
   Y = CircuitPlayground.motionY();
   Z = CircuitPlayground.motionZ();
 
-  // Serial.print("X: ");
-  // Serial.print(X);
-  // Serial.print("  Y: ");
-  // Serial.print(Y);
-  // Serial.print("  Z: ");
-  // Serial.println(Z);
 
+  // normalize data in each axis 
+  float scaled_x = constrain(X / SENSOR_RANGE, -1.0, 1.0);
+  float scaled_y = constrain(Y / SENSOR_RANGE, -1.0, 1.0);
+  float scaled_z = constrain(Z / SENSOR_RANGE, -1.0, 1.0);
 
+  // first combine, then one FFT
+  double mag = sqrt(scaled_x * scaled_x + scaled_y * scaled_y +
+                    scaled_z * scaled_z );
 
-  // put inside the array inputData
+  real[i] = mag;
+  imag[i] = 0;
+  i++; 
 
-  float raw = X;
-  float scaled = raw / SENSOR_RANGE; // scale to -1.0 to 1.0
+  if (i == SAMPLES) {
+    // perform FFT
+    FFT.windowing(real, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+    FFT.compute(real, imag, SAMPLES, FFT_FORWARD);
+    FFT.complexToMagnitude(real, imag, SAMPLES);
 
-  // prevent overflow
-  if (scaled > 1.0) scaled = 1.0;
-  if (scaled < -1.0) scaled = -1.0;
+    // calculate energy in tremor and dyskinesia bands
+    double tremorEnergy = 0;
+    double dyskinesiaEnergy = 0; 
+    double testEnergy = 0;
+    for (int j = 1; j < SAMPLES / 2; j++){
+      double freq = (j * SAMPLING_FREQ) / SAMPLES;
+      if (freq >= 3 && freq <= 7){
+        testEnergy += real[j];
+        Serial.print("Current Energy: ");
+        Serial.println(testEnergy); 
+      }
 
-  // int16_t range from -32768 to 32767, convert from float to int16_t
-  int16_t q15_val = (int16_t)(scaled * 32767);
+      // if (freq >= 3 && freq <= 5) {
+      //   tremorEnergy += real[j];
+      // } else if (freq > 5 && freq <= 7){
+      //   dyskinesiaEnergy += real[j];
+      // }
+    }
 
-  inputData[i] = q15_val;
-  i++;
-
-  if (i == 128){
-    ZeroFFT(inputData, DATA_SIZE);  //run the FFT
     i = 0;
   }
-    delay(1000);
+
+  delay(10); // determines Sampling frequency of 100Hz
 
 }
